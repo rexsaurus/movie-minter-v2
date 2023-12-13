431eba2fcb9c04c0de07cc61b4e11e0782fef1e9
rexsaurus <77756242+rexsaurus@users.noreply.github.com>
2023-12-13 22:09:14 +0000

    cors fix

--- api/replicate.js
+++ api/replicate.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Replicate from "replicate";
+import axios from "axios";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
@@ -19,24 +19,29 @@ app.post("/generate-video", async (req, res) => {
     // Extract prompt and API key from the request body
     const { prompt, apiKey } = req.body;
 
-    // Initialize Replicate client with the provided API key
-    const replicate = new Replicate({
-      auth: apiKey,
-    });
+    // Prepare the data for the POST request
+    const postData = {
+      version: "4e9283b2df49fad2dcf95755",
+      input: {
+        prompt: prompt,
+        // Additional parameters can be added here
+      },
+    };
 
     // Make the API call to Replicate
-    const output = await replicate.run(
-      "cjwbw/damo-text-to-video:4e9283b2df49fad2dcf95755",
+    const response = await axios.post(
+      "https://api.replicate.com/v1/predictions",
+      postData,
       {
-        input: {
-          prompt, // Use the prompt from the request
-          // Set other parameters as needed
+        headers: {
+          Authorization: `Token ${apiKey}`,
+          "Content-Type": "application/json",
         },
       },
     );
 
     // Send the response back to the client
-    res.status(200).json({ videoUrl: output });
+    res.status(200).json(response.data);
   } catch (error) {
     console.error("Error generating video:", error);
     res.status(500).json({ error: error.message });
