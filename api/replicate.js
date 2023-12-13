f4a903317f23fecea72cf6a4ed472af60106032e
rexsaurus <77756242+rexsaurus@users.noreply.github.com>
2023-12-13 23:49:34 +0000

    updating the server to add livepeer

--- api/replicate.js
+++ api/replicate.js
@@ -1,48 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import cors from "cors";
+import { Livepeer } from "livepeer";
 import Replicate from "replicate";
+import dotenv from "dotenv";
 
-// Load environment variables
 dotenv.config();
 
-// Create an express application
 const app = express();
-
-// Use CORS middleware
 app.use(cors());
-
-// Parse JSON bodies
 app.use(bodyParser.json());
 
-// Define the port
 const PORT = process.env.PORT || 5001;
 
-// POST route to handle video generation requests
+// POST route for video generation
 app.post("/generate-video", async (req, res) => {
   try {
-    // Extract prompt and API key from the request body
     const { prompt, apiKey } = req.body;
+    const replicate = new Replicate({ auth: apiKey });
 
-    // Initialize Replicate client with the provided API key
-    const replicate = new Replicate({
-      auth: apiKey,
-    });
-
-    // Create a prediction using the Replicate client
     let prediction = await replicate.deployments.predictions.create(
-      "rexsaurus", // Replace with your actual deployment name
-      "movie-minter-v2", // Replace with your actual version
-      {
-        input: { prompt },
-      },
+      "rexsaurus",
+      "movie-minter-v2",
+      { input: { prompt } },
     );
-
-    // Wait for the prediction to complete
     prediction = await replicate.wait(prediction);
 
-    // Send the prediction output back to the client
     res.status(200).json(prediction.output);
   } catch (error) {
     console.error("Error generating video:", error);
@@ -50,7 +33,19 @@ app.post("/generate-video", async (req, res) => {
   }
 });
 
-// Start the server
+// POST route to handle Livepeer upload requests
+app.post("/request-upload-url", async (req, res) => {
+  try {
+    const { fileName, livePeerApiKey } = req.body;
+    const livepeer = new Livepeer(livePeerApiKey);
+    const response = await livepeer.requestAssetUpload({ name: fileName });
+    res.status(200).json(response);
+  } catch (error) {
+    console.error("Error requesting asset upload:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
