c204fd6e4335a840ff7426cc503dee1e7743a1d1
rexsaurus <77756242+rexsaurus@users.noreply.github.com>
2023-12-21 22:27:46 +0000

    updating livepeer logic for a test

--- api/replicate.js
+++ api/replicate.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { Livepeer } from "livepeer";
 import Replicate from "replicate";
 import dotenv from "dotenv";
+import { TypeT } from "livepeer/dist/models/components";
 
 dotenv.config();
 
@@ -33,13 +34,39 @@ app.post("/generate-video", async (req, res) => {
   }
 });
 
-// POST route to handle Livepeer upload requests
 app.post("/request-upload-url", async (req, res) => {
   try {
     const { fileName, livePeerApiKey } = req.body;
-    const livepeer = new Livepeer(livePeerApiKey);
-    const response = await livepeer.requestAssetUpload({ name: fileName });
-    res.status(200).json(response);
+
+    const sdk = new Livepeer({
+      apiKey: livePeerApiKey,
+    });
+
+    const response = await sdk.asset.create({
+      name: fileName,
+      staticMp4: true,
+      playbackPolicy: {
+        type: TypeT.Jwt,
+        webhookId: "3e02c844-d364-4d48-b401-24b2773b5d6c",
+        webhookContext: {
+          //foo: "string",
+        },
+      },
+      creatorId: "string", // Replace with actual creator ID
+      storage: {
+        //ipfs: "string", // Replace with actual IPFS string if used
+      },
+      url: `https://s3.amazonaws.com/my-bucket/path/${fileName}`,
+      encryption: {
+        //encryptedKey: "string", // Replace with actual encrypted key if used
+      },
+    });
+
+    if (response.statusCode === 200) {
+      res.status(200).json(response.data);
+    } else {
+      res.status(response.statusCode).json({ error: response.data });
+    }
   } catch (error) {
     console.error("Error requesting asset upload:", error);
     res.status(500).json({ error: error.message });
